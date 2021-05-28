90e28ba8cc3e1a4bf0dc9d412345ca030031e380
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-05-28 22:20:45 +0000

    Add simple pixel manipulation

---
+++ pixel_manipulation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <hls_stream.h>
+#include <ap_axi_sdata.h>
+
+using namespace std;
+
+typedef ap_axis<24,0,0,0> pkt_t; 
+
+
+
+void pixel(
+
+		hls::stream< pkt_t > &din,
+		hls::stream< pkt_t > &dout,
+
+) {
+	#pragma HLS INTERFACE ap_ctrl_none port=return
+	#pragma HLS INTERFACE axis port=din
+	#pragma HLS INTERFACE axis port=dout
+
+	pkt_t pkt;
+	din.read(pkt);
+
+	    for(int i=0; i<=2; i++){
+            
+	        pkt.data[i] = 255; 
+            
+	    }
+
+	dout.write(pkt);
+}
+
