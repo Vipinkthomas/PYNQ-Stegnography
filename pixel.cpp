2d8d9adb9306f2c1be6208156439c9963f381e32
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-06-04 01:23:57 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -4,33 +4,37 @@
 
 using namespace std;
 
-typedef ap_axis<32,0,0,0> pkt_t; 
+typedef ap_axis<32,0,0,0> pkt_t;
 
 
 
 void pixel(
-		ap_int<32> &w,
-        ap_int<32> &len,
+		ap_int<32> w,
 		hls::stream< pkt_t > &din,
 		hls::stream< pkt_t > &dout
 ) {
 	#pragma HLS INTERFACE ap_ctrl_none port=return
 	#pragma HLS INTERFACE s_axilite port=w
-    #pragma HLS INTERFACE s_axilite port=len
 	#pragma HLS INTERFACE axis port=din
 	#pragma HLS INTERFACE axis port=dout
 
-	pkt_t pkt;
-	din.read(pkt);
+	pkt_t pkt=din.read();
+	static int count=0;
 
-	// if(pkt.data){
+	
 
-	len++;
-	// }
-
-	if(len==w){
+	if(counter==w){
 		pkt.data*=2;
 	}
+	else if(counter< w){
+		count++;
+	}
+	else{
+		counter=0;
+	}
+	
+
 
 	dout.write(pkt);
+
 }
