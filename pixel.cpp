94aecb12015e1b22271ff58615c1d37753807891
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-06-04 12:29:26 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -5,31 +5,26 @@
 using namespace std;
 
 typedef ap_axis<32,0,0,0> pkt_t;
-
+int count = 0;
 
 
 void pixel(
-		ap_int<32> w,
+		ap_int<32> position,
 		hls::stream< pkt_t > &din,
 		hls::stream< pkt_t > &dout
 ) {
 	#pragma HLS INTERFACE ap_ctrl_none port=return
-	#pragma HLS INTERFACE s_axilite port=w
+	#pragma HLS INTERFACE s_axilite port=position
 	#pragma HLS INTERFACE axis port=din
 	#pragma HLS INTERFACE axis port=dout
 
 	pkt_t pkt=din.read();
-	static int count=0;
-
 	
+	if(count == position){
+		pkt.data -= 1;
 
-	if(counter==w){
-		pkt.data*=2;
-		counter=0;
-
-	}
-	else{
-	count++;
+	}else{
+		count++;
 	}
 	// pending: have to make count=0 when TLAST signal is active -  for w not in the range of 0 to n(size of the array) 
 
