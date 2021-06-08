328c6c3a42e2fa7733ed21109fc56e624380cb8e
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-06-08 15:08:42 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -47,11 +47,10 @@ void pixel(
 	if(count_streams == 0){
 		
 		// DecToHex(charachter_in_decimal);
-		// jj = s[1];
+		jj = 'g';//s[1];
 		// jjj = s[0];
 		// s12 = jj + jjj;
-		char f;
-		f= 'g';
+		
 
 		// hexToBin(s12);
 
