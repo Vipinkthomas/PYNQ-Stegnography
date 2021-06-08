3fda68effb9dc1e9c6522d8c402ae60d3b66b8ca
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-06-08 14:25:48 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -21,9 +21,9 @@ static string vv = "";
 static string vvv = "";
 static int cnnt = 0;
 
-int decimalToBinary(int N);
-string DecToHex(int decimalNum);
-void hexToBin(string hexa);
+// int decimalToBinary(int N);
+// string DecToHex(int decimalNum);
+// void hexToBin(string hexa);
 
 
 
@@ -84,115 +84,116 @@ void pixel(
 	dout.write(pkt);
 
 }
-int decimalToBinary(int N)
-{
+//****************************************************************************************************
+// int decimalToBinary(int N)
+// {
  
-    // To store the binary number
-    ull B_Number = 0;
-    int cnt = 0;
-    while (N != 0) {
-        int rem = N % 2;
-        ull c = pow(10, cnt);
-        B_Number += rem * c;
-        N /= 2;
-        // Count used to store exponent value
-        cnt++;
-    }
+//     // To store the binary number
+//     ull B_Number = 0;
+//     int cnt = 0;
+//     while (N != 0) {
+//         int rem = N % 2;
+//         ull c = pow(10, cnt);
+//         B_Number += rem * c;
+//         N /= 2;
+//         // Count used to store exponent value
+//         cnt++;
+//     }
  
-    return B_Number;
-}
+//     return B_Number;
+// }
 
-string DecToHex(int decimalNum)
-{
-     int rem, i=0;
-    char hexaDecimalNum[2];
+// string DecToHex(int decimalNum)
+// {
+//      int rem, i=0;
+//     char hexaDecimalNum[2];
     
-    while(decimalNum!=0)
-    {
-        rem = decimalNum%16;
-        if(rem<10)
-            rem = rem+48;
-        else
-            rem = rem+55;
-        hexaDecimalNum[i] = rem;
-        i++;
-        decimalNum = decimalNum/16;
-    }
+//     while(decimalNum!=0)
+//     {
+//         rem = decimalNum%16;
+//         if(rem<10)
+//             rem = rem+48;
+//         else
+//             rem = rem+55;
+//         hexaDecimalNum[i] = rem;
+//         i++;
+//         decimalNum = decimalNum/16;
+//     }
     
-    for(i=i-1; i>=0; i--)
-        s[i] = hexaDecimalNum[i];
+//     for(i=i-1; i>=0; i--)
+//         s[i] = hexaDecimalNum[i];
     
-    return hexaDecimalNum;
-}
+//     return hexaDecimalNum;
+// }
 
-void hexToBin(string hexa){ 
-    long int i = 0;
+// void hexToBin(string hexa){ 
+//     long int i = 0;
     
-   while (hexa[i]){
-      switch (hexa[i]){
-      case '0':
-         l +=  "0000";
-         break;
-      case '1':
-         l +=  "0001";
-         break;
-      case '2':
-         l +=  "0010";
-         break;
-      case '3':
-         l +=  "0011";
-         break;
-      case '4':
-         l +=  "0100";
-         break;
-      case '5':
-         l +=  "0101";
-         break;
-      case '6':
-         l +=  "0110";
-         break;
-      case '7':
-         l +=  "0111";
-         break;
-      case '8':
-         l +=  "1000";
-         break;
-      case '9':
-         l +=  "1001";
-         break;
-      case 'A':
-      case 'a':
-         l +=  "1010";
-         break;
-      case 'B':
-      case 'b':
-         l +=  "1011";
-         break;
-      case 'C':
-      case 'c':
-         l +=  "1100";
-         break;
-      case 'D':
-      case 'd':
-         l +=  "1101";
-         break;
-      case 'E':
-      case 'e':
-         l +=  "1110";
-         break;
-      case 'F':
-      case 'f':
-         l +=  "1111";
-         break;
-      default:
-        cout<<"S";
+//    while (hexa[i]){
+//       switch (hexa[i]){
+//       case '0':
+//          l +=  "0000";
+//          break;
+//       case '1':
+//          l +=  "0001";
+//          break;
+//       case '2':
+//          l +=  "0010";
+//          break;
+//       case '3':
+//          l +=  "0011";
+//          break;
+//       case '4':
+//          l +=  "0100";
+//          break;
+//       case '5':
+//          l +=  "0101";
+//          break;
+//       case '6':
+//          l +=  "0110";
+//          break;
+//       case '7':
+//          l +=  "0111";
+//          break;
+//       case '8':
+//          l +=  "1000";
+//          break;
+//       case '9':
+//          l +=  "1001";
+//          break;
+//       case 'A':
+//       case 'a':
+//          l +=  "1010";
+//          break;
+//       case 'B':
+//       case 'b':
+//          l +=  "1011";
+//          break;
+//       case 'C':
+//       case 'c':
+//          l +=  "1100";
+//          break;
+//       case 'D':
+//       case 'd':
+//          l +=  "1101";
+//          break;
+//       case 'E':
+//       case 'e':
+//          l +=  "1110";
+//          break;
+//       case 'F':
+//       case 'f':
+//          l +=  "1111";
+//          break;
+//       default:
+//         cout<<"S";
          
-      }
-   i++;
-   }
+//       }
+//    i++;
+//    }
 
-}
-
+// }
+//********************************************************************************************
 
 
 
