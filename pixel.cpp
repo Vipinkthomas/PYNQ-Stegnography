51227d70c4ace78615bb13ee8d40dc2600e042a0
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-06-16 22:11:54 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -78,4 +78,13 @@ long long toAscii(int c) {
     return bin;
 }
 
-
+int convertBinInt(long long n) {
+    int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
