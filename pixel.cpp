0713fba033113c5751233ee5d60682cbe08dba18
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-06-21 11:39:46 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -63,7 +63,9 @@ void pixel(ap_int<32> selector,
 
         case 1:
 
-            if((count_streams >= 3 * (position1 - 1)) && (count_streams < 3 * (position2))){
+            ascii=0;
+            if((count_streams >= 3 * (position1 - 1)) && (count_streams < 3 * (position2)-1)){
+                
                 
                 decrypt(pkt.data);
             
@@ -115,12 +117,15 @@ final_char= final_char*10+bit;
 }
 
 int convertBinInt(long long n) {
-    int dec = 0, i = 0, rem;
+    int dec = 0, i = 7,b, rem;
     while (n != 0) {
         rem = n % 10;
-        n /= 10;
+        b=pow(10,i);
+        n =n % b;
+         printf(" %lld\n", n);
         dec += rem * pow(2, i);
-        ++i;
+        printf(" %d\n", dec);
+        --i;
     }
     return dec;
 }
