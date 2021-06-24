876cfa38f0e92681a0526b01283d7a1f77dc5a94
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-06-24 18:50:52 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
-#include "pixel.hpp"
+#include <pixel.hpp>
+
 
 int main() {
-	pkt_t din, dout;
-    auto* a=50;
-    pixel(0,1,3,27,a,din,dout);
+
+	stream streamIn;
+	stream streamOut;
+
+
+	for (int y = 0; y < 2; y++) {
+	    for (int x = 0; x < 2; x++) {
+	    	for (int z = 0;z < 2; z++){
+	    	pkt_t tmp;
+	        tmp.data = 255;
+	        tmp.user = (x == 0) && (y == 0)&&(z==0);
+	        tmp.last = (x == 26);
+	        streamIn.write(tmp);
+	    	}
+	    }
+	}
+	ap_int<32> a;
+    ap_int<32> selector,position1,position2,stream_count;
+    selector=0;
+    position1=1;
+    position2=1;
+    stream_count=1;
+    a=50;
+    pixel(selector,position1,position2,stream_count,a,streamIn,streamOut);
+
+    for (int y = 0; y < 2; y++) {
+    	    for (int x = 0; x < 2; x++) {
+    	    	for (int z = 0;z < 2; z++){
+            pkt_t tmp = streamOut.read();
+            printf("%d",tmp.data);
+        }
+    }
+    }
+
+
+    return 0;
 
 }
+
+
 
 
