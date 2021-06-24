d7635dd3e3925f3f0abfe01b5a88f41f64231ba4
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-06-24 18:51:24 +0000

    Update pixel.hpp

--- pixel.hpp
+++ pixel.hpp
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include<math.h>
 #include <string.h>
+#include <hls_stream.h>
+#include <ap_axi_sdata.h>
 
 using namespace std;
-
 typedef ap_axis<32,0,0,0> pkt_t;
-static int count_streams = 0;
-static long long charIn=0;
-static long long final_char=0;
-int addNum=0;
+typedef hls::stream< pkt_t > stream;
+void pixel(ap_int<32> selector,
+		ap_int<32> position1,
+		ap_int<32> position2,
+		ap_int<32> stream_count,
+		ap_int<32> ascii,
+		stream &din,
+		stream &dout
+
+);
 
 long long convert(int n);
 int convertBinInt(long long n);
