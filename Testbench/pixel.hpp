41bf5e49644c5f078b259b93b872215059ed47da
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-06-30 10:11:07 +0000

    Update pixel.hpp

--- Testbench/pixel.hpp
+++ Testbench/pixel.hpp
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
-#include <string.h>
+#include <math.h>
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 
+//#include <string.h>
+//#include <array>
+
+//const auto N = 1<<2;  // array size
+//using vec_t = std::array<char, N>;
+
 using namespace std;
-typedef ap_axis<32,0,0,0> pkt_t;
+typedef ap_axis<32,1,1,1> pkt_t;
+
 
 typedef hls::stream< pkt_t > stream;
-void pixel(ap_int<32> selector,
-		ap_int<32> position1,
-		ap_int<32> position2,
-		ap_int<32> stream_count,
-		ap_int<32> ascii,
-		stream &din,
-		stream &dout
 
-);
+void pixel(ap_int<32> &in_decimal,
+	        ap_int<32> selector,
+			ap_int<32> position1,
+			ap_int<32> position2,
+			hls::stream< pkt_t > &din,
+			hls::stream< pkt_t > &dout
+
+	);
 
 long long convert(int n);
 int convertBinInt(long long n);
 void decrypt(int data);
+int getDecimal(int n);
