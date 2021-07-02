9e822fbfe928ef1307b97616c167f4a7116b2031
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-07-02 19:02:44 +0000

    setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='Embedded Acceleration',
+      version='0.1',
+      description='pynq-jupyter',
+      url='https://mygit.th-deg.de/vt16684/embedded-acceleration',
+      author='vipin and Majd',
+      author_email='vipin.thomas@stud.th-deg.de',
+      license='MIT',
+      packages=[],
+      install_requires=['bs4==0.0.1',
+                        'requests==2.25.1',
+                         'panda==0.3.1',
+                         'plotly==5.1.0',
+                         'matplotlib==2.1.1',
+                         'pillow==5.1.1',
+                         'numpy==1.16.0',
+                         'pynq==2.6.0'],
+      zip_safe=False)
+
+
+
