4c3e5fba987c453e842783dd54eb34a8a0bed5c5
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-07-02 16:57:28 +0000

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -7,9 +7,9 @@ void pixel(apint &in_decimal,
 		   stream &dout
 ) {
 	    #pragma HLS INTERFACE ap_ctrl_none port=return
-        #pragma HLS INTERFACE s_axilite port=in_decimal     // ascii values for the characters(word)
-        #pragma HLS INTERFACE s_axilite port=selector       // to select the mode (encoding or decoding)
-	    #pragma HLS INTERFACE s_axilite port=stream_count   // size of data stream 
+        #pragma HLS INTERFACE s_axilite port=in_decimal         // ascii values for the characters(word)
+        #pragma HLS INTERFACE s_axilite port=selector           // to select the mode (encoding or decoding)
+	    #pragma HLS INTERFACE s_axilite port=stream_count       // size of data stream 
 	    #pragma HLS INTERFACE axis port=din
 	    #pragma HLS INTERFACE axis port=dout
 
